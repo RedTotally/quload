d7c6307472ad9eea6767951f47529648ba63941b
TheRed <94051573+RedTotally@users.noreply.github.com>
2025-02-12 15:22:51 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
     formData.append("file", renamedFile);
 
     try {
-      const response = await fetch("https://quload.dotwebshosting.com/upload", {
+      const response = await fetch("https://quload.com/upload", {
         method: "POST",
         body: formData,
       });
